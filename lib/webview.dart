5455ec023c5aae8105089bac9937cf9cd4033dcf
kctsohil <kctsohil@gmail.com>
2023-11-19 23:55:27 +0545

    first commit

---
+++ lib/webview.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class webview extends StatelessWidget {
+  const webview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
